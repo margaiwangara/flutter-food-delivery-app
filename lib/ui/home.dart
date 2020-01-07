72f2fbcabbdf05a67d38461294df7576464aa4a4
margaiwangara <margaiwangara@gmail.com>
2020-01-07 14:20:23 +0300

    finished initial design of the header

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Homepage extends StatefulWidget {
   @override
@@ -20,8 +21,51 @@ class _HomepageState extends State<Homepage> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         color: Theme.of(context).primaryColor.withOpacity(0.16),
-        child: Text('this is a text'),
+        child: _homePageHeader(),
       )
     ]));
+  }
+
+  _homePageHeader() {
+    return Container(
+        child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(
+          height: 50.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('Kenya',
+                  style: GoogleFonts.montserrat(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  width: 45.0,
+                  height: 45.0,
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).accentColor,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(0.25),
+                            blurRadius: 10.0,
+                            offset: new Offset(0.0, 0.75))
+                      ]),
+                  child: Center(child: Icon(Icons.search, color: Colors.white)),
+                ),
+              )
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
